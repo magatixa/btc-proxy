cb7668f9bd58b2c895b0756e2ad0e3d4f89779fe
magatixa <58227383+magatixa@users.noreply.github.com>
2025-06-01 23:48:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Servidor proxy CoinDesk online!"
+    return "Servidor proxy CoinGecko online!"
 
 @app.route("/btc")
 def btc():
     try:
-        url = "https://api.coindesk.com/v1/bpi/currentprice/BRL.json"
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl"
         resposta = requests.get(url)
         data = resposta.json()
-        valor_btc = data["bpi"]["BRL"]["rate_float"]
+        valor_btc = data["bitcoin"]["brl"]
         return str(valor_btc)
     except Exception as e:
         return f"erro: {e}"

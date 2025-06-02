7d696b9e67b0b02dc24ce08a558bd4c61d217456
magatixa <58227383+magatixa@users.noreply.github.com>
2025-06-02 00:04:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Servidor proxy Brasil Bitcoin online!"
+    return "Servidor proxy CoinDesk online!"
 
 @app.route("/btc")
 def btc():
     try:
-        url = "https://www.brasilbitcoin.com.br/API/ticker/BTC"
-        resposta = requests.get(url)
-        print("Status code:", resposta.status_code)
-        print("Conteúdo:", resposta.text)
-        return resposta.text  # Mostra o conteúdo real retornado pela API
+        url = "https://api.coindesk.com/v1/bpi/currentprice/BRL.json"
+        headers = {"User-Agent": "Mozilla/5.0"}
+        resposta = requests.get(url, headers=headers)
+        data = resposta.json()
+        valor_btc = data["bpi"]["BRL"]["rate_float"]
+        return str(valor_btc)
     except Exception as e:
         return f"erro: {e}"

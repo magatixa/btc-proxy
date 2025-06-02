3d38e4d2fe81ee82c49020e8f258e8ab9d05c618
magatixa <58227383+magatixa@users.noreply.github.com>
2025-06-01 23:36:39 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Servidor proxy CoinDesk online!"
+
+@app.route("/btc")
+def btc():
+    try:
+        url = "https://api.coindesk.com/v1/bpi/currentprice/BRL.json"
+        resposta = requests.get(url)
+        data = resposta.json()
+        valor_btc = data["bpi"]["BRL"]["rate_float"]
+        return str(valor_btc)
+    except Exception as e:
+        return f"erro: {e}"
